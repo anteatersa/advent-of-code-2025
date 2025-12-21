3d906326630af01bf73bc7a4fbefa3f7e1918104
Adam Cox <adam@umlungu.co.uk>
2025-12-21 16:52:45 +0000

    solved part b

--- 5/part-b.c
+++ 5/part-b.c
@@ -14,11 +14,6 @@ struct FreshRange {
     long long end;
 };
 
-// Return 2 ints
-void parse_range(const char *buffer, long *a, long *b) {
-    sscanf(buffer, "%i-%i", &a, &b);
-}
-
 // Sort the fresh_ids array
 int compare_ranges(const void *a, const void *b) {
     // 1. Cast the generic void pointers to your struct type
@@ -38,15 +33,12 @@ int compare_ranges(const void *a, const void *b) {
 
 int main(void)
 {
-    FILE *file_ptr = fopen("test.input","r");
-    //FILE *file_ptr = fopen("part-a.input","r");
+    //FILE *file_ptr = fopen("test.input","r");
+    FILE *file_ptr = fopen("part-a.input","r");
     char line_buffer[128];
     enum Stage stage = RANGES; 
-    //long long fresh_ids[1000000]; // fresh products
     struct FreshRange fresh_ids[10000]; // array of structs
     int fresh_ids_count = 0;
-    long long available_ids[1000000]; // available products
-    int available_ids_count = 0;
     long long int_a;
     long long int_b;
     long long available = 0;
@@ -72,43 +64,34 @@ int main(void)
     qsort(fresh_ids, fresh_ids_count, sizeof(struct FreshRange), compare_ranges);
 
     // Loop over fresh ids and then loop over all fresh ids going forward only
-    // find any overlaps and remove from self
-    // i.e. adjust start or end to remove overlap
-    // 2-6 / 4-12 / 16-18
-    // 2-4 / 1-2 / 16-18
+    // when the next range is deemed to be overlapping then we will set
+    // current end to the end of that range
+    long long current_start = fresh_ids[0].start;
+    long long current_end = fresh_ids[0].end;
 
-    for (int i = 0; i < fresh_ids_count; i++) {
+    for (int i = 1; i < fresh_ids_count; i++) {
+        printf("==== current range: %lld - %lld\n", fresh_ids[i].start, fresh_ids[i].end);
+        printf("  -- current start/end: %lld - %lld\n", current_start, current_end);
+        printf("  -- next range: %lld - %lld\n", fresh_ids[i].start, fresh_ids[i].end);
 
-        printf("==== checking range: %lld - %lld\n", fresh_ids[i].start, fresh_ids[i].end);
-        
-        for (int i2 = i+1; i2 < fresh_ids_count; i2++) {
-            if (
-                fresh_ids[i].end >= fresh_ids[i2].start 
-                && fresh_ids[i].start <= fresh_ids[i2].start
-            ) {
-                if (DEBUG) printf("adjust end of id: %lld to %lld\n", fresh_ids[i].end, fresh_ids[i2].start - 1);
-                fresh_ids[i].end = fresh_ids[i2].start - 1;
+        if (current_end >= fresh_ids[i].start && fresh_ids[i].end > current_end) {
+            printf("    -- overlap detected\n");
+            current_end = fresh_ids[i].end;
+        } else {
+            // Catch any where the range sites inside current start and end
+            if (fresh_ids[i].start >= current_start && fresh_ids[i].end <= current_end) {
+                printf("ignored range\n");
+            } else {
+                printf("    --  ranges do not overlap, adding to total: %lld\n", (current_end - current_start) + 1); 
+                available += (current_end - current_start) + 1; // inclusive
+                current_start = fresh_ids[i].start;
+                current_end = fresh_ids[i].end;
             }
-
-            if (
-                fresh_ids[i].start <= fresh_ids[i2].end 
-                && fresh_ids[i].end >= fresh_ids[i2].end 
-            ) {
-                if (DEBUG) printf("adjust start of id: %lld to %lld\n", fresh_ids[i].start, fresh_ids[i2].end + 1);
-                fresh_ids[i].start = fresh_ids[i2].end + 1;
-            }
-
         }
     }
-    // TODO: do we need to remove duplicate ranges
 
-    // Now add up all fresh ranges
-    for (int i = 0; i < fresh_ids_count; i++) {
-        printf("==== checking range: %lld - %lld\n", fresh_ids[i].start, fresh_ids[i].end);
-        printf("adding: %lld\n", (fresh_ids[i].end - fresh_ids[i].start + 1));
-        available += (fresh_ids[i].end - fresh_ids[i].start) + 1; // inclusive
-    }
-
+    // Gotcha, remember to add the last range
+    available += (current_end - current_start) + 1; // inclusive
 
     printf("Number of ingredients available: %lld\n", available);
 }
